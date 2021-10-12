65b1890966554ded3c094aeba638aae22b00a5a6
MahmudsGit <golammahmud1314@gmail.com>
2021-10-13 01:25:25 +0600

    working with Admin Dashboard

---
+++ resources/views/layouts/frontend/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }} - @yield('title')</title>
+
+    <!-- COMMON CSS -->
+    <link href="{{ asset('assets/frontend/css/bootstrap.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/frontend/css/swiper.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/frontend/css/ionicons.css') }}" rel="stylesheet">
+
+    <!-- PAGE WISE CSS -->
+    @stack('css')
+
+</head>
+<body>
+
+    @include('layouts.frontend.partials.header')
+
+    @yield('content')
+
+    @include('layouts.frontend.partials.footer')
+
+    <script src="{{ asset('assets/frontend/js/jquery-3.1.1.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/tether.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/scripts.js') }}"></script>
+
+    <!-- PAGE WISE JS -->
+    @stack('js')
+</body>
+</html>
